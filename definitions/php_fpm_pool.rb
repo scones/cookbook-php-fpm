69330c43c5865f975cc2d0cd6a2aa8d3a630ac88
Dirk Gustke <dirk.gustke@vmpublishing.com>
2016-07-04 12:42:46 +0200

    use_override_param - use the param

--- definitions/php_fpm_pool.rb
+++ definitions/php_fpm_pool.rb
@@ -19,7 +19,7 @@
 
 define :php_fpm_pool, :template => "pool.conf.erb", :enable => true do
 
-  pool_name = params[:name]
+  pool_name = params[:pool_name] || params[:name]
 
   conf_file = "#{node['php-fpm']['pool_conf_dir']}/#{pool_name}.conf"
 
